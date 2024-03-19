18a202b9c18e0e5c3f3335ccdc5b254fe5f5c745
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-19 08:12:32 +0300

    Add messages service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MessagesService } from './services/messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [MessagesService],
 })
 export class AppComponent {
+  messages: string[] = [];
+
+  constructor(private messagesService: MessagesService) {
+    this.messages = messagesService.getMessages();
+  }
   title = 'angular-note-taking-app';
 }
