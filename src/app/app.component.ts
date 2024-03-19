48b11c9c053f5b8338b3158385b7092fcd8c9b7b
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-19 10:58:37 +0300

    Display posts to the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessagesService } from './services/messages.service';
 
 @Component({
@@ -7,11 +7,27 @@ import { MessagesService } from './services/messages.service';
   styleUrls: ['./app.component.scss'],
   providers: [MessagesService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   messages: string[] = [];
+  posts: any[] = [];
 
   constructor(private messagesService: MessagesService) {
     this.messages = messagesService.getMessages();
   }
+
+  ngOnInit(){
+    // this.messagesService.getPosts().subscribe(response => {
+    //   this.posts = response;
+    // }, (error) => {
+    //   console.error(error);
+    // }) 
+    //subscribe has been deprecated
+
+    this.messagesService.getPosts().subscribe({
+      next: (response) => {this.posts = response;},
+      error: (error) => {console.error(error);}
+    })
+  }
+
   title = 'angular-note-taking-app';
 }
