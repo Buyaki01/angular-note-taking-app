98ff15a6c8b6e9bcb6a4aeb102e1117c65d2a831
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-25 14:04:04 +0300

    Add note-list and note-form components, add note service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteFormComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
