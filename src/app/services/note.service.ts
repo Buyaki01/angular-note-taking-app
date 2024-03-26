cf8d2bf416f483244ebb0c7c94ca132d112348b9
Buyaki01 <rittahbuyaki@gmail.com>
2024-03-26 10:27:06 +0300

    Add createNote function

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Note } from '../interfaces/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteService {
+  private notes: Note[] = [];
+  private notesSubject = new BehaviorSubject<Note[]>([])
 
   constructor() { }
+
+  createNote(note: Note): void {
+    note.id = this.notes.length;
+    this.notes.push(note);
+    this.notesSubject.next(this.notes);
+  }
 }
